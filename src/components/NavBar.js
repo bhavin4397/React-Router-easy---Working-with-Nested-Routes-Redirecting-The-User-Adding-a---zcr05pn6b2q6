731b16818542231f63d8df773b5892b8cc3243dc
bhavin4397 <bhavinpatidar4397@gmail.com>
2022-10-07 13:57:58 +0530

    updateCode

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,7 +7,7 @@ export const NavBar = () =>{
         <nav>
             <ul>
                 <li><NavLink className='index-link' to='/'>Index</NavLink></li>
-                <li><NavLink className='home-link' to='/home'></NavLink></li>
+                <li><NavLink className='home-link' to='/home'>Home</NavLink></li>
             </ul>
         </nav>
     )
